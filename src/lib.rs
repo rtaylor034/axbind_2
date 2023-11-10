279304b038c7f060618fae0e24d6592dee72d6ce
gfunction <ryan.mm.taylor@gmail.com>
2023-11-10 11:18:36 -0800

    config.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,5 @@
+use toml_context::*;
+use std::path::{Path, PathBuf};
+use thiserror::Error;
 pub mod args;
+pub mod config;
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use anyhow::{Context, Error};
 use axbind::*;
 fn program() -> Result<(), Error> {
     let program_args = args::ProgramArgs::from_runinfo(gfunc::run::RunInfo::get_from_env());
-    let config_root = gfunc::for_until(&program_args.config_paths, |path| toml_context::TableRoot::from_file_path(path).ok())
+    let root_config = gfunc::for_until(&program_args.config_paths, |path| toml_context::TableRoot::from_file_path(path).ok())
         .with_context(|| format!("No valid config files could be found -- paths checked: {:#?}\n (check for invalid toml syntax)",
                                  &program_args.config_paths))?;
+
     Ok(())
 }
 
