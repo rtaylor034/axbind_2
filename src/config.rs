727b7857e6d6e9025ca0644878b77a170ce9db43
gfunction <ryan.mm.taylor@gmail.com>
2023-11-10 12:33:16 -0800

    masterconfig

--- src/config.rs
+++ src/config.rs
@@ -3,11 +3,14 @@ use crate:: {
     TableResultOptional,
     TableGetError,
     TableHandle,
+    PotentialValueHandle,
     TableResult,
     PathBuf,
     Path,
+    Error,
+    Context,
 };
-
+#[derive(Clone, Debug)]
 pub struct MasterConfig<'t> {
     pub map_directory: &'t String,
     pub function_directory: &'t String,
@@ -16,16 +19,71 @@ pub struct MasterConfig<'t> {
     pub layer_options: LayerOptions<'t>,
     pub meta_options: MetaOptions<'t>,
 }
-
+#[derive(Clone, Debug)]
 pub struct GroupOptions<'t> {
     pub axbind_file_format: Option<&'t String>,
 }
+#[derive(Clone, Debug)]
 pub struct LayerOptions<'t> {
     pub escape_char: Option<char>,
     pub key_format: Option<&'t String>,
 }
+#[derive(Clone, Debug)]
 pub struct MetaOptions<'t> {
     pub escape_char: Option<char>,
     pub wildcard_char: Option<char>,
     _p: core::marker::PhantomData<&'t str>,
+}
+#[derive(Error, Debug)]
+pub enum ConfigError {
+    #[error(transparent)]
+    TableGet(#[from] TableGetError),
+    #[error("{0}")]
+    Misc(String),
+}
+impl<'st> MasterConfig<'st> {
+    pub fn from_table<'t>(handle: TableHandle<'t>) -> Result<MasterConfig<'t>, ConfigError> {
+        Ok(MasterConfig {
+            map_directory: extract_value!(String, handle.get("map_directory"))?,
+            function_directory: extract_value!(String, handle.get("function_directory"))?,
+            tag_directory: extract_value!(String, handle.get("tag_directory"))?,
+            group_options: GroupOptions::from_table_forced(handle.traverse(&["options", "group"])?)?,
+            layer_options: LayerOptions::from_table_forced(handle.traverse(&["options", "layer"])?)?,
+            meta_options: MetaOptions::from_table_forced(handle.traverse(&["options", "meta"])?)?,
+        })
+    }
+}
+impl<'st> GroupOptions<'st> {
+    pub fn from_table_forced<'t>(handle: TableHandle<'t>) -> Result<GroupOptions<'t>, ConfigError> {
+        Ok(GroupOptions {
+            axbind_file_format: Some(extract_value!(String, handle.get("axbind_file_format"))?),
+        })
+    }
+}
+impl<'st> LayerOptions<'st> {
+    pub fn from_table_forced<'t>(handle: TableHandle<'t>) -> Result<LayerOptions<'t>, ConfigError> {
+        Ok(LayerOptions {
+            escape_char: Some(extract_char(handle.get("escape_char"))?),
+            key_format: Some(extract_value!(String, handle.get("key_format"))?),
+        })
+    }
+}
+impl<'st> MetaOptions<'st> {
+    pub fn from_table_forced<'t>(handle: TableHandle<'t>) -> Result<MetaOptions<'t>, ConfigError> {
+        Ok(MetaOptions {
+            escape_char: Some(extract_char(handle.get("escape_char"))?),
+            wildcard_char: Some(extract_char(handle.get("wildcard_char"))?),
+            _p: core::marker::PhantomData,
+        })
+    }
+}
+fn extract_char(handle: PotentialValueHandle) -> Result<char, ConfigError> {
+    let raw = extract_value!(String, handle.clone())?.as_str();
+    match raw.len() == 1 {
+        true => Ok(raw.chars().next().unwrap()),
+        false => Err(ConfigError::Misc(format!(
+            "value for '{}' must be exactly 1 character",
+            handle.context
+        ))),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,24 @@ use anyhow::{Context, Error};
 use axbind::*;
 fn program() -> Result<(), Error> {
     let program_args = args::ProgramArgs::from_runinfo(gfunc::run::RunInfo::get_from_env());
-    let root_config = gfunc::for_until(&program_args.config_paths, |path| toml_context::TableRoot::from_file_path(path).ok())
+    let root_master = gfunc::for_until(&program_args.config_paths, |path| toml_context::TableRoot::from_file_path(path).ok())
         .with_context(|| format!("No valid config files could be found -- paths checked: {:#?}\n (check for invalid toml syntax)",
-                                 &program_args.config_paths))?;
-
+                                 program_args.config_paths))?;
+    let config_master = config::MasterConfig::from_table(root_master.handle())?;
+    eprintln!(" >> MASTER CONFIG :: {:#?}", config_master);
     Ok(())
 }
 
 fn main() {
     match program() {
-        Err(e) => eprintln!("[FATAL!] {}", e),
+        Err(e) => { 
+            eprintln!("[FATAL!]");
+            let mut spaces = String::new();
+            for (i, cause) in e.chain().enumerate() {
+                spaces.push(' ');
+                eprintln!(" ({}){}> {}", i, spaces, cause);
+            }
+        }
         Ok(_) => eprintln!(" >> OK <<"),
     }
 }
