7589c86ffc242f4cb0f4f00e6494ebf4b0a8e7d1
gfunction <ryan.mm.taylor@gmail.com>
2023-11-10 01:11:42 -0800

    lets get this started yea

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,8 @@
-mod args;
+pub mod args;
+pub enum MainErr {
+}
+impl std::fmt::Display for MainErr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use axbind::*;
+fn program() -> Result<(), MainErr> {
+    let program_args = args::ProgramArgs::from_runinfo(gfunc::run::RunInfo::get_from_env());
+    let config_root = gfunc::for_until(&program_args.config_paths, |path| toml_context::TableRoot::from_file_path(path).ok());
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    match program() {
+        Err(e) => eprintln!("[FATAL!] {}", e),
+        Ok(_) => eprintln!(" >> OK <<"),
+    }
 }
