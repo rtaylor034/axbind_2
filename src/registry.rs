bc7a83b040e1de6cadc09783a246effc234dea77
gfunction <ryan.mm.taylor@gmail.com>
2023-11-11 17:07:36 -0800

    from_handles() on Registry

--- src/registry.rs
+++ src/registry.rs
@@ -17,9 +17,8 @@ pub trait RegistryItem {
     fn verify() -> Result<(), Error>;
     fn is_verified() -> bool;
 }
-pub struct Registry<T: RegistryItem> {
-    registry: HashMap<String, T>,
-    roots: Vec<TableRoot>,
+pub struct Registry<'t, T: RegistryItem> {
+    registry: HashMap<&'t String, T>,
 }
 pub struct BindMap<'t> {
     verified: bool,
@@ -32,9 +31,18 @@ pub struct BindFunction<'t> {
     shell: &'t str,
     command: &'t str,
 }
-impl<T: RegistryItem> Registry<T> {
-    pub fn from_handles<'t, I: IntoIterator<Item = TableHandle<'t>>>(handles: I) -> Result<Registry<T::Identity<'t>>, Error> {
-        todo!()
+impl<'st, T: RegistryItem> Registry<'st, T> {
+    pub fn from_handles<'t, I: IntoIterator<Item = TableHandle<'t>>>(handles: I) -> Registry<'t, T::Identity<'t>> {
+        let name_key = format!("axbind_{}", T::ITEM_TYPE);
+        let registry: HashMap<&String, T::Identity<'t>> = HashMap::from_iter(
+            handles.into_iter()
+            .filter_map(|handle| 
+                warn!(extract_value!(String, handle.get(name_key.as_str()))
+                    .with_context(|| format!("No '{}' key found in file '{}'. (file skipped)", name_key, handle.context))).ok()
+                    .map(|name| (name, T::construct_unverified(handle)))));
+        Registry {
+            registry,
+        }
     }
     pub fn get(key: &str) -> Result<T, Error> {
         todo!();
